0357b265c9f0862064179ad9cf43e6208f115c43
aiden30015 <loolloo080313@gmail.com>
2025-07-23 19:00:01 +0900

    💄 :: Colors 스타일 코드 추가

---
+++ lib/core/config/constans/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class WispColors {
+  static const Color nightBlack = Color(0xFF0F0F23);
+  static const Color deepBlue1 = Color(0xFF1A1A2E);
+  static const Color deepBlue2 = Color(0xFF16213E);
+  static const Color deepBlue3 = Color(0xFF0F3460);
+  static const Color deepPurple = Color(0xFF533483);
+}
