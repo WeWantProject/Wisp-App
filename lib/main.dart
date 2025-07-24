dab7a4ae0769ec8a6c905f03fadebaa84c71e36b
aiden30015 <loolloo080313@gmail.com>
2025-07-24 19:57:30 +0900

    ✨ :: Splsh 스크린 뜨게 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:wisp/core/di/injection.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:wisp/core/config/constans/theme.dart';
+import 'package:wisp/core/config/di/injection.dart';
+import 'package:wisp/core/config/router/route..dart';
+import 'package:wisp/presentation/splash/screens/splash_screen.dart';
 
 Future<void> main () async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  runApp(Wisp());
+  runApp(const ProviderScope(child: Wisp(),));
 }
 
 
@@ -13,6 +18,39 @@ class Wisp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      builder: (context, child) {
+        return MaterialApp.router(
+          routerDelegate: AppRouter.router.routerDelegate,
+          routeInformationParser: AppRouter.router.routeInformationParser,
+          routeInformationProvider: AppRouter.router.routeInformationProvider,
+          debugShowCheckedModeBanner: false,
+          theme: wispTheme,
+        );
+      },
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+
+  int selectedIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashScreen();
   }
 }
