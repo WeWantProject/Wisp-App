37a0e004484fdc681a5559aa11e202a4c73ef295
aiden30015 <loolloo080313@gmail.com>
2025-07-24 19:56:45 +0900

    💄 :: Color 추가

--- lib/core/config/constans/colors.dart
+++ lib/core/config/constans/colors.dart
@@ -6,4 +6,7 @@ class WispColors {
   static const Color deepBlue2 = Color(0xFF16213E);
   static const Color deepBlue3 = Color(0xFF0F3460);
   static const Color deepPurple = Color(0xFF533483);
+  static const Color grey = Color(0xD96A6E83);
+  static const Color lightSkyBlue = Color(0xFFB1D5FF);
+
 }
