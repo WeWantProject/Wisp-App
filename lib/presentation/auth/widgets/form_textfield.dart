efd917a5c465c5887b5756331767faa4e491f39c
aiden30015 <loolloo080313@gmail.com>
2025-07-25 10:56:36 +0900

    💄 :: 로그인 및 회원가입 텍스트 필드 퍼블리싱

---
+++ lib/presentation/auth/widgets/form_textfield.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:wisp/core/config/constans/colors.dart';
+
+class FormTextfield extends HookWidget {
+  final String hintText;
+  final String labelText;
+  final TextEditingController controller;
+  final IconData leadingIcon;
+  final bool isPassword;
+
+  const FormTextfield({
+    super.key,
+    required this.hintText,
+    required this.labelText,
+    required this.controller,
+    required this.leadingIcon,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final obscureText = useState(isPassword);
+
+    void togglePasswordVisibility() {
+      obscureText.value = !obscureText.value;
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          labelText,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          onChanged: (value) => controller.text = value,
+          obscureText: obscureText.value,
+          controller: controller,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: WispColors.lightSkyBlue,
+            ),
+            prefixIcon: Icon(
+              leadingIcon,
+              color: WispColors.lightSkyBlue,
+            ),
+            suffixIcon: isPassword
+                ? IconButton(
+                    icon: Icon(
+                      obscureText.value
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: WispColors.lightSkyBlue,
+                    ),
+                    onPressed: () {
+                      togglePasswordVisibility();
+                    },
+                  )
+                : null,
+            filled: true,
+            fillColor: WispColors.grey,
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Colors.white),
+            ),
+          ),
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+          cursorColor: Colors.white,
+        ),
+      ],
+    );
+  }
+}
