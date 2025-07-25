48c64089b88161ac502500725130ab75c9cd272e
aiden30015 <loolloo080313@gmail.com>
2025-07-25 10:56:50 +0900

    ✨ :: 라우터 의존성 추가

--- lib/core/config/di/injection.config.dart
+++ lib/core/config/di/injection.config.dart
@@ -11,6 +11,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:wisp/core/config/router/route..dart' as _i431;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -18,11 +19,12 @@ extension GetItInjectableX on _i174.GetIt {
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
+    gh.singleton<_i431.AppRouter>(() => _i431.AppRouter());
     return this;
   }
 }
