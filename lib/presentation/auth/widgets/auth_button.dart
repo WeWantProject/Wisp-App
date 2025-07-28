5419d0b6c784d2a22e999afe14389a3f8877ec07
aiden30015 <loolloo080313@gmail.com>
2025-07-29 07:54:14 +0900

    💄 :: Auth Button 공통 위젯 퍼블리싱

---
+++ lib/presentation/auth/widgets/auth_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:wisp/core/config/constans/colors.dart';
+import 'package:wisp/core/config/constans/theme.dart';
+
+class AuthButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+
+  const AuthButton({super.key, required this.onPressed, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 50,
+      decoration: BoxDecoration(
+          color: WispColors.deepBlue2,
+          borderRadius: BorderRadius.circular(12)),
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
