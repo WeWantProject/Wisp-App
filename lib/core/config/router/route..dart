ec167291b4a7f528464a9161fc7cb37dc3ece15d
aiden30015 <loolloo080313@gmail.com>
2025-08-07 22:00:15 +0900

    ♻️ :: Route 구조 리팩터링

--- lib/core/config/router/route..dart
+++ lib/core/config/router/route..dart
@@ -1,24 +1,17 @@
 import 'package:go_router/go_router.dart';
-import 'package:injectable/injectable.dart';
 import 'package:wisp/presentation/auth/screens/auth_screen.dart';
 import 'package:wisp/presentation/splash/screens/splash_screen.dart';
 
-class RouterPaths {
-  static const String splash = '/splash';
-  static const String auth = '/auth';
-}
-
-@Singleton()
 class AppRouter {
-  GoRouter get router => GoRouter(
-    initialLocation: RouterPaths.splash,
+  static final GoRouter router = GoRouter(
+    initialLocation: '/splash',
     routes: [
       GoRoute(
-        path: RouterPaths.splash,
+        path: '/splash',
         builder: (context, state) => const SplashScreen(),
       ),
       GoRoute(
-        path: RouterPaths.auth,
+        path: '/login',
         builder: (context, state) => const AuthScreen(),
       ),
     ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wisp/core/config/constans/theme.dart';
+import 'package:wisp/core/config/constants/theme.dart';
 import 'package:wisp/core/config/di/injection.dart';
 import 'package:wisp/core/config/router/route..dart';
-import 'package:wisp/presentation/splash/screens/splash_screen.dart';
 
 Future<void> main () async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  runApp(const ProviderScope(child: Wisp(),));
+  runApp(const ProviderScope(child: Wisp()));
 }
-
 
 class Wisp extends StatelessWidget {
   const Wisp({super.key});
@@ -18,34 +16,9 @@ class Wisp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-          routerDelegate: AppRouter.router.routerDelegate,
-          routeInformationParser: AppRouter.router.routeInformationParser,
-          routeInformationProvider: AppRouter.router.routeInformationProvider,
-          debugShowCheckedModeBanner: false,
-          theme: wispTheme,
-        );
-      }
-}
-
-class Main extends StatefulWidget {
-  const Main({super.key});
-
-  @override
-  State<Main> createState() => _MainState();
-}
-
-class _MainState extends State<Main> {
-
-  int selectedIndex = 0;
-
-  void onTap(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const SplashScreen();
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
+      theme: wispTheme,
+    );
   }
 }
