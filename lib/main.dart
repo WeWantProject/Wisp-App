51b7af71ec7d37882737b379ac5c93201d5484b0
aiden30015 <loolloo080313@gmail.com>
2025-08-08 18:15:42 +0900

    ✏️ :: 파일명 오타 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wisp/core/config/constants/theme.dart';
 import 'package:wisp/core/config/di/injection.dart';
-import 'package:wisp/core/config/router/route..dart';
+import 'package:wisp/core/config/router/route.dart';
 
 Future<void> main () async {
   WidgetsFlutterBinding.ensureInitialized();
