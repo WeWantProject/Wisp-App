607dda6bcfc19ade95e656ede42c14ef2d5b138c
aiden30015 <loolloo080313@gmail.com>
2025-08-10 21:41:57 +0900

    ✨ :: BaseScaffold에 bottomnavgiationbar 추가

--- lib/core/config/constants/base_scaffold.dart
+++ lib/core/config/constants/base_scaffold.dart
@@ -4,8 +4,9 @@ import 'package:wisp/core/config/constants/theme.dart';
 class BaseScaffold extends StatelessWidget {
   final Widget child;
   final AppBar? appBar;
+  final BottomNavigationBar? bottomNavigationBar;
 
-  const BaseScaffold({super.key, required this.child, this.appBar});
+  const BaseScaffold({super.key, required this.child, this.appBar, this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,7 @@ class BaseScaffold extends StatelessWidget {
         ),
         child: child,
       ),
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
 }
