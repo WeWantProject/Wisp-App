77b7a8ed4310949d64a04aee7888e77611651f01
aiden30015 <loolloo080313@gmail.com>
2025-08-10 21:38:16 +0900

    ✨ :: Shell Router 추가

--- lib/core/config/router/route.dart
+++ lib/core/config/router/route.dart
@@ -1,5 +1,8 @@
 import 'package:go_router/go_router.dart';
+import 'package:wisp/core/config/widgets/bottom_navigation.dart';
 import 'package:wisp/presentation/auth/screens/auth_screen.dart';
+import 'package:wisp/presentation/chat/screens/chat_list_screen.dart';
+import 'package:wisp/presentation/profile/screens/profile_screen.dart';
 import 'package:wisp/presentation/splash/screens/splash_screen.dart';
 
 class AppRouter {
@@ -11,9 +14,24 @@ class AppRouter {
         builder: (context, state) => const SplashScreen(),
       ),
       GoRoute(
-        path: '/login',
+        path: '/auth',
         builder: (context, state) => const AuthScreen(),
       ),
+      ShellRoute(
+        builder: (context, state, child) {
+          return BottomNavigation(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: '/chat',
+            builder: (context, state) => const ChatListScreen(),
+          ),
+          GoRoute(
+            path: '/profile',
+            builder: (context, state) => const ProfileScreen(),
+          ),
+        ],
+      )
     ],
   );
 }
