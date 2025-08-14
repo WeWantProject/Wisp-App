d6d78bf87283be5fa9d0b8098d418e99ed10d242
aiden30015 <loolloo080313@gmail.com>
2025-08-14 09:03:02 +0900

    💄 :: Profile appbar 퍼블리싱

--- lib/core/config/widgets/bottom_navigation.dart
+++ lib/core/config/widgets/bottom_navigation.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:wisp/core/config/constants/base_scaffold.dart';
 import 'package:wisp/core/config/constants/colors.dart';
+import 'package:wisp/presentation/chat/screens/chat_list_screen.dart';
+import 'package:wisp/presentation/chat/widgets/chat_list_appbar.dart';
+import 'package:wisp/presentation/profile/screens/profile_screen.dart';
+import 'package:wisp/presentation/profile/widgets/profile_appbar.dart';
 
 class BottomNavigation extends StatefulWidget {
-  final Widget child;
-  final PreferredSizeWidget? appBar;
-
-  const BottomNavigation({
-    super.key,
-    required this.child,
-    this.appBar,
-  });
+  const BottomNavigation({super.key});
 
   @override
   State<BottomNavigation> createState() => _BottomNavigationState();
@@ -20,25 +16,27 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigationState extends State<BottomNavigation> {
   int _currentIndex = 0;
 
-  final _tabs = [
-    '/chat',
-    '/profile',
+  final _pages = const [
+    ChatListScreen(),
+    ProfileScreen(userName: '황지훈', statusMessage: '멋진 남자 좋아해용 ㅎㅎ',),
+  ];
+
+  final List<PreferredSizeWidget> _appBars = const [
+    ChatListAppbar(),
+    ProfileAppbar(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
-      appBar: widget.appBar,
+      appBar: _appBars[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: WispColors.deepBlue3,
         currentIndex: _currentIndex,
         selectedItemColor: Colors.white,
         unselectedItemColor: WispColors.grey,
         onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-          context.go(_tabs[index]);
+          setState(() => _currentIndex = index);
         },
         items: const [
           BottomNavigationBarItem(
@@ -51,7 +49,14 @@ class _BottomNavigationState extends State<BottomNavigation> {
           ),
         ],
       ),
-      child: widget.child,
+      child: Stack(
+        children: List.generate(_pages.length, (index) {
+          return Offstage(
+            offstage: _currentIndex != index,
+            child: _pages[index],
+          );
+        }),
+      ),
     );
   }
 }
