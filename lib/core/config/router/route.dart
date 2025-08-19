c42df2c5138c66e7ea3b6d5029a972a14b7db9dc
aiden30015 <loolloo080313@gmail.com>
2025-08-19 23:59:41 +0900

    ♻️ :: Route 구조 리팩터링

--- lib/core/config/router/route.dart
+++ lib/core/config/router/route.dart
@@ -3,49 +3,63 @@ import 'package:wisp/core/config/widgets/bottom_navigation.dart';
 import 'package:wisp/presentation/auth/screens/auth_screen.dart';
 import 'package:wisp/presentation/chat/screens/chat_list_screen.dart';
 import 'package:wisp/presentation/chat/screens/chat_screen.dart';
+import 'package:wisp/presentation/setting/screens/add_freind_screen.dart';
 import 'package:wisp/presentation/splash/screens/splash_screen.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
     initialLocation: '/splash',
     routes: [
+
+      /// Splash
       GoRoute(
         path: '/splash',
         name: 'splash',
         builder: (context, state) => const SplashScreen(),
       ),
+
+      /// Auth
       GoRoute(
         path: '/auth',
         name: 'auth',
         builder: (context, state) => const AuthScreen(),
       ),
+
+      /// Main (BottomNavigation)
       GoRoute(
         path: '/main',
         name: 'main',
         builder: (context, state) => const BottomNavigation(),
       ),
+
+      /// Chat
       GoRoute(
-          path: '/chat',
-          name: 'chat',
-          builder: (context, state) {
-            return const ChatListScreen();
-          }),
+        path: '/chat',
+        name: 'chat',
+        builder: (context, state) => const ChatListScreen(),
+      ),
       GoRoute(
         path: '/chat/:chatId',
         name: 'chatDetail',
         builder: (context, state) {
           final chatId = state.pathParameters['chatId']!;
-          final userName =
-              state.extra is Map ? (state.extra as Map)['userName'] : '사용자 이름';
-          final isOnline = state.extra is Map
-              ? (state.extra as Map)['isOnline'] ?? false
-              : false;
+          final extra = state.extra as Map<String, dynamic>? ?? {};
+          final userName = extra['userName'] ?? '사용자 이름';
+          final isOnline = extra['isOnline'] ?? false;
+
           return ChatScreen(
             chatId: chatId,
             userName: userName,
             isOnline: isOnline,
           );
         },
+      ),
+
+      /// Add Friend (Profile → AddFriendScreen 바로 이동)
+      GoRoute(
+        path: '/add-friend',
+        name: 'addFriend',
+        builder: (context, state) => const AddFreindScreen(),
       ),
     ],
   );
