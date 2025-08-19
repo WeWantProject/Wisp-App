55ea2c44fd3970375f9e134c2c30e0dd69986168
aiden30015 <loolloo080313@gmail.com>
2025-08-19 23:34:31 +0900

    💄 :: Setting appbar 퍼블리싱

---
+++ lib/presentation/setting/widgets/setting_appbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingAppbar extends StatelessWidget {
+  final String title;
+
+  const SettingAppbar({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios_new, color: Colors.white),
+        onPressed: () => context.pop(),
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white
+        )),
+      );
+  }
+}
