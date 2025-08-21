ac8c2541cfe40cd9e943c686ed564a9e07e505b2
aiden30015 <loolloo080313@gmail.com>
2025-08-21 10:03:51 +0900

    💄 :: InviteLinkCard 퍼블리싱

--- lib/presentation/setting/screens/add_freind_screen.dart
+++ lib/presentation/setting/screens/add_freind_screen.dart
@@ -13,17 +13,20 @@ class AddFreindScreen extends StatelessWidget {
         title: '친구 초대',
       ),
       child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: ConstrainedBox(
           constraints: BoxConstraints(
             minHeight: MediaQuery.of(context).size.height,
             minWidth: MediaQuery.of(context).size.width,
           ),
           child: const Column(
+            spacing: 20,
             children: [
               SizedBox(
                 height: 20,
               ),
               _QrCodeAddFriend(),
+              _InviteLinkCard(),
             ],
           ),
         ),
@@ -39,7 +42,7 @@ class _QrCodeAddFriend extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       child: Column(
-        spacing: 10,
+        spacing: 20,
         children: [
           const Text(
             'QR 코드로 초대',
@@ -57,6 +60,110 @@ class _QrCodeAddFriend extends StatelessWidget {
               fontSize: 16,
               color: WispColors.lightSkyBlue,
               fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _InviteLinkCard extends StatelessWidget {
+  const _InviteLinkCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 10,
+        children: [
+          const Text(
+            '초대 링크 공유',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: WispColors.lightSkyBlue,
+                width: 0.3,
+              ),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      '내 초대 코드',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                        vertical: 4,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Text(
+                        'WISP2024',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: WispColors.lightSkyBlue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.blue,
+                        WispColors.deepPurple,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.share,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 16),
+                      Text(
+                        '링크 복사',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           )
         ],
