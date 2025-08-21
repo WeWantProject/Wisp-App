76dd35e298001c75e8cdabcd804536a34dd8df61
aiden30015 <loolloo080313@gmail.com>
2025-08-21 13:00:14 +0900

    💄 :: 친구 코드 입력 카드 퍼블리싱

--- lib/presentation/setting/screens/add_freind_screen.dart
+++ lib/presentation/setting/screens/add_freind_screen.dart
@@ -28,9 +28,43 @@ class AddFreindScreen extends StatelessWidget {
               ),
               _QrCodeAddFriend(),
               _InviteLinkCard(),
+              _FriendCodeInput(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _QrCodeAddFriend extends StatelessWidget {
+  const _QrCodeAddFriend();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        spacing: 20,
+        children: [
+          const Text(
+            'QR 코드로 초대',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          Container(
+              width: 200,
+              height: 200,
+              color: Colors.white,
+              child: const SizedBox.shrink()), // Qr 코드로 대체 예정
+          const Text(
+            '친구가 이 QR 코드를 스캔하면 Wisp에 초대됩니다',
+            style: TextStyle(
+              fontSize: 16,
+              color: WispColors.lightSkyBlue,
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
       ),
     );
   }
@@ -116,46 +150,105 @@ class _InviteLinkCard extends StatelessWidget {
   }
 }
 
-class _QrCodeAddFriend extends StatelessWidget {
-  const _QrCodeAddFriend();
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Column(
-        spacing: 20,
-        children: [
-          const Text(
-            'QR 코드로 초대',
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          Container(
-              width: 200,
-              height: 200,
-              color: Colors.white,
-              child: const SizedBox.shrink()), // Qr 코드로 대체 예정
-          const Text(
-            '친구가 이 QR 코드를 스캔하면 Wisp에 초대됩니다',
-            style: TextStyle(
-              fontSize: 16,
-              color: WispColors.lightSkyBlue,
-              fontWeight: FontWeight.w600,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
 class _FriendCodeInput extends StatelessWidget {
   const _FriendCodeInput();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(),
+    return InviteCard(
+      title: '친구 코드 입력',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 10,
+        children: [
+          const Text(
+            '친구의 초대 코드를 입력하세요',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                      color: WispColors.lightSkyBlue,
+                      width: 0.3,
+                    ),
+                    color: Colors.grey.withOpacity(0.2),
+                  ),
+                  child: const TextField(
+                    cursorColor: Colors.white,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 12),
+                      hintText: '예: WISP2024',
+                      hintStyle: TextStyle(
+                        color: WispColors.lightSkyBlue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Colors.green,
+                    ),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.person_add,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        '추가',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            '친구로부터 받은 8자리 코드를 입력하세요.',
+            style: TextStyle(
+              fontSize: 14,
+              color: WispColors.lightSkyBlue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
