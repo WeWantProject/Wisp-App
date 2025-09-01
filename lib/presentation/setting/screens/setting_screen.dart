88aeb3cac3d0d307b97256938f2aaac211d0a05f
이주언 <loolloo080313@gmail.com>
2025-09-01 17:49:05 +0900

    💄 :: BasicSettings 퍼블리싱

--- lib/presentation/setting/screens/setting_screen.dart
+++ lib/presentation/setting/screens/setting_screen.dart
@@ -4,13 +4,18 @@ import 'package:wisp/core/config/constants/base_scaffold.dart';
 import 'package:wisp/core/config/constants/colors.dart';
 import 'package:wisp/presentation/setting/widgets/setting_appbar.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
   Widget build(BuildContext context) {
     return const BaseScaffold(
-      appBar: SettingAppbar(
+      appBar: const SettingAppbar(
         title: "설정",
       ),
       child: SafeArea(
@@ -18,31 +23,94 @@ class SettingScreen extends StatelessWidget {
           padding: EdgeInsetsGeometry.symmetric(vertical: 14, horizontal: 16),
           child: Column(
             children: [
-              _SettingTile(
-                title: "기본 설정",
-                icon: Icon(
-                  Icons.settings_outlined,
-                  color: Colors.amber,
-                ),
-                children: [
-                  _SettingItem(
-                    icon: Icon(
-                      Icons.notifications_outlined,
-                      color: Colors.orange,
-                    ),
-                    title: "알림",
-                    content: "메시지 및 통화 알림",
-                    active: Switch(
-                      value: true,
-                      onChanged: null,
-                    ),
-                  ),
-                ],
-              ),
+              _BasicSettings(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _BasicSettings extends StatefulWidget {
+  const _BasicSettings({super.key});
+
+  @override
+  State<_BasicSettings> createState() => __BasicSettingsState();
+}
+
+class __BasicSettingsState extends State<_BasicSettings> {
+  bool _isOn = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return _SettingTile(
+      title: "기본 설정",
+      icon: const Icon(
+        Icons.settings_outlined,
+        color: Colors.amber,
+      ),
+      children: [
+        _SettingItem(
+          icon: const Icon(
+            Icons.notifications_outlined,
+            color: Colors.orange,
+          ),
+          title: "알림",
+          content: "메시지 및 통화 알림",
+          active: Switch(
+            value: _isOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isOn = value;
+              });
+            },
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey.shade400,
+            activeTrackColor: Colors.black,
+          ),
+        ),
+        const Gap(16),
+        _SettingItem(
+          icon: Icon(
+            _isOn ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+            color: Colors.purpleAccent,
+          ),
+          title: "다크 테마",
+          content: "어두운 테마 사용",
+          active: Switch(
+            value: _isOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isOn = value;
+              });
+            },
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey.shade400,
+            activeTrackColor: Colors.black,
+          ),
+        ),
+        const Gap(16),
+        _SettingItem(
+          icon: const Icon(
+            Icons.chat_bubble_outline,
+            color: Colors.green,
+          ),
+          title: "읽음 확인",
+          content: "메시지 읽음 표시",
+          active: Switch(
+            value: _isOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isOn = value;
+              });
+            },
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey.shade400,
+            activeTrackColor: Colors.black,
+          ),
+        ),
+      ],
     );
   }
 }
