1122e3d2559ba8ace6bf9d88a8a2d76defde568c
이주언 <loolloo080313@gmail.com>
2025-09-01 18:05:48 +0900

    💄 :: 배경색 변경

--- lib/presentation/setting/screens/setting_screen.dart
+++ lib/presentation/setting/screens/setting_screen.dart
@@ -3,6 +3,7 @@ import 'package:gap/gap.dart';
 import 'package:wisp/core/config/constants/base_scaffold.dart';
 import 'package:wisp/core/config/constants/colors.dart';
 import 'package:wisp/presentation/setting/widgets/setting_appbar.dart';
+import 'package:wisp/presentation/setting/widgets/setting_dropdown.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -15,15 +16,17 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return const BaseScaffold(
-      appBar: const SettingAppbar(
+      appBar: SettingAppbar(
         title: "설정",
       ),
       child: SafeArea(
         child: Padding(
           padding: EdgeInsetsGeometry.symmetric(vertical: 14, horizontal: 16),
           child: Column(
+            spacing: 20,
             children: [
               _BasicSettings(),
+              _InformationSettings(),
             ],
           ),
         ),
@@ -108,6 +111,90 @@ class __BasicSettingsState extends State<_BasicSettings> {
             inactiveThumbColor: Colors.white,
             inactiveTrackColor: Colors.grey.shade400,
             activeTrackColor: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _InformationSettings extends StatefulWidget {
+  const _InformationSettings({super.key});
+
+  @override
+  State<_InformationSettings> createState() => __InformationSettingsState();
+}
+
+class __InformationSettingsState extends State<_InformationSettings> {
+  bool _isOn = true;
+  String selectedValue = '친구만'; // 초기값
+
+  final List<String> options = ['친구만', '전체공개', '비공개'];
+
+  @override
+  Widget build(BuildContext context) {
+    return _SettingTile(
+      title: "개인정보 보호",
+      icon: const Icon(
+        Icons.shield_outlined,
+        color: Colors.green,
+      ),
+      children: [
+        _SettingItem(
+          icon: const Icon(
+            Icons.visibility_outlined,
+            color: Colors.blue,
+          ),
+          title: "마지막 접속 시간",
+          content: "다른 사용자에게 표시",
+          active: Switch(
+            value: _isOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isOn = value;
+              });
+            },
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey.shade400,
+            activeTrackColor: Colors.black,
+          ),
+        ),
+        const Gap(16),
+        _SettingItem(
+          icon: const Icon(
+            Icons.people_alt_outlined,
+            color: Colors.green,
+          ),
+          title: "온라인 상태",
+          content: "현재 온라인 상태 표시",
+          active: Switch(
+            value: _isOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isOn = value;
+              });
+            },
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey.shade400,
+            activeTrackColor: Colors.black,
+          ),
+        ),
+        const Gap(16),
+        _SettingItem(
+          icon: const Icon(
+            Icons.chat_bubble_outline,
+            color: Colors.green,
+          ),
+          title: "읽음 확인",
+          content: "메시지 읽음 표시",
+          active: SettingDropdown(
+            options: options,
+            selectedValue: selectedValue,
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedValue = newValue!;
+              });
+            },
           ),
         ),
       ],
