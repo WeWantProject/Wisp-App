48103d59232912b6e19b57bee37da32a137f7d46
이주언 <loolloo080313@gmail.com>
2025-09-06 17:21:52 +0900

    ♻️ :: 파일 구조 변경

--- lib/core/config/di/configurations.dart
+++ lib/core/config/di/configurations.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:wisp/core/config/configurations.config.dart';
+import 'package:wisp/core/config/di/configurations.config.dart';
 
 final getIt = GetIt.instance;
 
