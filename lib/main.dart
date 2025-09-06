7c6392b789d7b6ee0501ec9d99560f44611977d7
이주언 <loolloo080313@gmail.com>
2025-09-06 17:36:37 +0900

    ✨ :: Env 파일 불러오는 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wisp/core/config/configurations.dart';
+import 'package:wisp/core/config/di/configurations.dart';
 import 'package:wisp/core/config/constants/theme.dart';
 import 'package:wisp/core/config/router/route.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
   runApp(const ProviderScope(child: Wisp()));
