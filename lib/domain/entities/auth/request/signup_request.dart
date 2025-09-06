bc948f89a21e66b7497a16709e0e7cb799485a4f
이주언 <loolloo080313@gmail.com>
2025-09-06 22:21:36 +0900

    ✨ :: SignUp request entity class

---
+++ lib/domain/entities/auth/request/signup_request.dart
@@ -0,0 +1,11 @@
+class SignupRequest {
+  final String userName;
+  final String phoneNumber;
+  final String password;
+
+  SignupRequest({
+    required this.userName,
+    required this.phoneNumber,
+    required this.password,
+  });
+}
