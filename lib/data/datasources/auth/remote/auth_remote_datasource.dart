dde6a2b70f5862c41bae22291107134f84be62a8
이주언 <loolloo080313@gmail.com>
2025-09-09 17:16:18 +0900

    ✨ :: Auth remote datasource class

---
+++ lib/data/datasources/auth/remote/auth_remote_datasource.dart
@@ -0,0 +1,47 @@
+import 'package:wisp/data/datasources/auth/remote/auth_api.dart';
+import 'package:wisp/data/models/auth/request/signin_request_dto.dart';
+import 'package:wisp/data/models/auth/request/signup_request_dto.dart';
+import 'package:wisp/data/models/auth/response/refresh_token_response_dto.dart';
+
+abstract class AuthRemoteDataSource {
+  Future<void> sendSms(String phoneNumber);
+  Future<void> verifyPhone(String phoneNumber, String code);
+  Future<void> signUp(SignUpRequestDto dto);
+  Future<void> signIn(SigninRequestDto dto);
+  Future<void> changePassword(String phoneNumber, String newPassword);
+  Future<RefreshTokenResponseDto> refreshToken(String refreshToken);
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final AuthApi api;
+
+  AuthRemoteDataSourceImpl(this.api);
+
+  @override
+  Future<void> sendSms(String phoneNumber) async {
+    await api.sendSms({"phoneNumber": phoneNumber});
+  }
+
+  @override
+  Future<void> verifyPhone(String phoneNumber, String code) async {
+    await api.verifyPhone({"phoneNumber": phoneNumber, "code": code});
+  }
+
+  @override
+  Future<void> signUp(SignUpRequestDto dto) => api.signUp(dto);
+
+  @override
+  Future<void> signIn(SigninRequestDto dto) => api.signIn(dto);
+
+  @override
+  Future<void> changePassword(String phoneNumber, String newPassword) async {
+    await api.changePassword({
+      "phoneNumber": phoneNumber,
+      "newPassword": newPassword,
+    });
+  }
+
+  @override
+  Future<RefreshTokenResponseDto> refreshToken(String refreshToken) =>
+      api.refreshToken({"refreshToken": refreshToken});
+}
