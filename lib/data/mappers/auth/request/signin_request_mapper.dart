0af95558f8fbdbadf6cb8fe3d9effd564427171a
이주언 <loolloo080313@gmail.com>
2025-09-09 12:03:33 +0900

    ✨ :: SignIn reqeust mapper class

---
+++ lib/data/mappers/auth/request/signin_request_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:wisp/data/models/auth/request/signin_request_dto.dart';
+import 'package:wisp/domain/entities/auth/signin_entity.dart';
+
+extension SiginEntityMapper on SignInEntity {
+  SignInEntity toEntity() {
+    return SignInEntity(
+      phoneNumber: phoneNumber,
+      password: password,
+    );
+  }
+}
+
+extension SignInRequestMapper on SigninRequestDto {
+  SigninRequestDto toDto() {
+    return SigninRequestDto(
+      phoneNumber: phoneNumber,
+      password: password,
+    );
+  }
+}
