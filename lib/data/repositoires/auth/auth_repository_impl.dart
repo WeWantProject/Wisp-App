fd678c0dd1155ba477d6090e61ee4538ffe8f6b4
이주언 <loolloo080313@gmail.com>
2025-09-09 17:47:09 +0900

    ✨ :: Auth repostory impl class

---
+++ lib/data/repositoires/auth/auth_repository_impl.dart
@@ -0,0 +1,47 @@
+import 'package:wisp/data/datasources/auth/remote/auth_remote_datasource.dart';
+import 'package:wisp/data/mappers/auth/request/signin_request_mapper.dart';
+import 'package:wisp/data/mappers/auth/request/signup_request_mapper.dart';
+import 'package:wisp/data/mappers/auth/response/token_response_mapper.dart';
+import 'package:wisp/domain/entities/auth/signin_entity.dart';
+import 'package:wisp/domain/entities/auth/signup_entity.dart';
+import 'package:wisp/domain/entities/auth/token_entity.dart';
+import 'package:wisp/domain/repositories/auth/auth_repository.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final AuthRemoteDataSourceImpl remoteDataSource;
+
+  AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<void> ChangePassword(String phoneNumber, String newPassword) async {
+    await remoteDataSource.changePassword(phoneNumber, newPassword);
+  }
+
+  @override
+  Future<TokenEntity> refreshToken(String refreshToken) async {
+    final response = await remoteDataSource.refreshToken(refreshToken);
+    return TokenResponseMapper.toEntity(response);
+  }
+
+  @override
+  Future<void> sendSms(String phoneNumber) {
+    return remoteDataSource.sendSms(phoneNumber);
+  }
+
+  @override
+  Future<void> signIn(SignInEntity signinEntity) async {
+    final dto = SigninRequestMapper.toDto(signinEntity);
+    await remoteDataSource.signIn(dto);
+  }
+
+  @override
+  Future<void> signUp(SignUpEntity signUpEntity) {
+    final dto = SignupRequestMapper.toDto(signUpEntity);
+    return remoteDataSource.signUp(dto);
+  }
+
+  @override
+  Future<void> verifyPhone(String code) async {
+    return remoteDataSource.verifyPhone(code);
+  }
+}
