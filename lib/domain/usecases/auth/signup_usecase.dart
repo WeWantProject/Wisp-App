2908cc1c4d19d68b78bc7577b89d1b9ccce261d5
이주언 <loolloo080313@gmail.com>
2025-09-10 19:35:26 +0900

    ♻️ :: 클래스 및 함수 이름 변경

--- lib/domain/usecases/auth/signup_usecase.dart
+++ lib/domain/usecases/auth/signup_usecase.dart
@@ -1,12 +1,12 @@
 import 'package:wisp/domain/entities/auth/signup_entity.dart';
 import 'package:wisp/domain/repositories/auth/auth_repository.dart';
 
-class SignupUsecase {
+class SignUpUseCase {
   final AuthRepository authRepository;
 
-  SignupUsecase(this.authRepository);
+  SignUpUseCase(this.authRepository);
 
-  Future<void> call(SignUpEntity signUpEntity) async {
+  Future<void> signUp(SignUpEntity signUpEntity) async {
     return await authRepository.signUp(signUpEntity);
   }
 }
