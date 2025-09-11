0dc46c858903a662e78bf742c86e5763b26998af
이주언 <loolloo080313@gmail.com>
2025-09-11 22:50:58 +0900

    ✨ :: Dio 기본 세팅 추가

--- lib/core/config/di/network_module.dart
+++ lib/core/config/di/network_module.dart
@@ -1,13 +1,24 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wisp/core/network/api_service.dart';
 import 'package:wisp/data/datasources/auth/remote/auth_api.dart';
 
 @module
 abstract class NetworkModule {
   @lazySingleton
-  Dio dio() => Dio();
+  Dio dio() => Dio(
+        BaseOptions(
+          baseUrl: dotenv.env['BASE_URL'] ?? '',
+          connectTimeout: const Duration(seconds: 5),
+          receiveTimeout: const Duration(seconds: 5),
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        ),
+      );
 
   @lazySingleton
   FlutterSecureStorage storage() => const FlutterSecureStorage();
