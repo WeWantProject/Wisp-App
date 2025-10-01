f0f5f861e028ea7703be0004407766ed1fb8cae4
이주언 <loolloo080313@gmail.com>
2025-10-01 14:49:24 +0900

    ✨ :: IsTokenExpired 상태 추가

--- lib/presentation/auth/state/auth_state.dart
+++ lib/presentation/auth/state/auth_state.dart
@@ -5,6 +5,7 @@ part 'auth_state.freezed.dart';
 @freezed
 abstract class AuthState with _$AuthState {
   const factory AuthState({
-    @Default([true,false]) List<bool> isLoginMode,
+    @Default([true, false]) List<bool> isLoginMode,
+    @Default(false) bool isTokenExpired,
   }) = _AuthState;
 }
